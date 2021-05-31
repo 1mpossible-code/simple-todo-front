425590fc28e393c40a2d31a0a1de706cf4d4894f
Max <linme00p@gmail.com>
2021-05-31 13:39:19 +0300

    Create TaskList component. Create TaskList component that implements some basic logic

---
+++ src/components/TaskList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>TaskList</div>
+  <div v-for="task in tasks" :key="task.id">
+    <tasks-list-item>
+      <template v-slot:content>{{ task.body }}</template>
+    </tasks-list-item>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
+import TasksListItem from "./TaskListItem.vue";
+
+export default {
+  name: "TaskList",
+  components: { TasksListItem },
+  setup() {
+    const store = useStore();
+    const tasks = ref([]);
+
+    onMounted(() => {
+      store.dispatch("tasks/fetch");
+      tasks.value = store.state.tasks.all;
+    });
+
+    return {
+      tasks,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
