0f92c637004e06ee193e0b0f5f8618c838c4b234
Max <linme00p@gmail.com>
2021-05-31 13:40:38 +0300

    Update App.vue. Update App.vue component to use TaskList component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="logo">Simple TODO app</div>
-  <create-task/>
+  <create-task />
+
+  <task-list />
 </template>
 
 <script>
-import CreateTask from "./components/CreateTask";
+import CreateTask from "./components/CreateTask.vue";
+import TaskList from "./components/TaskList.vue";
 
 export default {
   name: "App",
   components: {
     CreateTask,
+    TaskList,
   },
 };
 </script>
